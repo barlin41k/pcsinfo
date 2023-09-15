5df5f1ddf32bbe260d363dd11315be44d1963113
barlin41k <132909893+barlin41k@users.noreply.github.com>
2023-09-15 14:05:24 +0300

    0.1.0

---
+++ examples/from_user.py
@@ -0,0 +1,10 @@
+from pcsinfo import User
+
+user = User()
+username = computer.system_username
+
+"""
+This module is very easy to using. In this example,
+we created a class instance into "user" var.
+And then, we gotted a your PC user name.
+"""
