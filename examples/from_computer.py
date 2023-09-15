cfe77ee0c00b2a0b4cb98ccaf96520ea63c7f120
barlin41k <132909893+barlin41k@users.noreply.github.com>
2023-09-15 14:05:04 +0300

    0.1.0

---
+++ examples/from_computer.py
@@ -0,0 +1,10 @@
+from pcsinfo import Computer
+
+computer = Computer()
+ram = computer.ram
+
+"""
+This module is very easy to using. In this example,
+we created a class instance into "computer" var.
+And then, we gotted a your PC RAM.
+"""
